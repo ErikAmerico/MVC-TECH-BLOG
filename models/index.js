d58278fe3fc8eaf5455d6ceae085b129e668a4e6
Erik Olson <111093132+ErikAmerico@users.noreply.github.com>
2023-07-17 16:40:39 -0400

    continued working on views. updated model relationships

--- models/index.js
+++ models/index.js
@@ -1,12 +1,27 @@
+// const User = require('./User');
+// const Blog = require('./Blog');
+
+// User.hasMany(Blog, {
+//     foreignKey: 'writer_id',
+// });
+
+// Blog.belongsTo(User, {
+//     foreignKey: 'writer_id',
+// });
+
+// module.exports = { User, Blog };
+
 const User = require('./User');
-const Blog = require('./Blog');
+const Post = require('./Post');
+const Comment = require('./Comment');
 
-User.hasMany(Blog, {
-    foreignKey: 'writer_id',
-});
+User.hasMany(Post, { foreignKey: 'user_id', onDelete: 'CASCADE' });
+User.hasMany(Comment, { foreignKey: 'user_id', onDelete: 'CASCADE' });
 
-Blog.belongsTo(User, {
-    foreignKey: 'writer_id',
-});
+Post.belongsTo(User, { foreignKey: 'user_id' });
+Post.hasMany(Comment, { foreignKey: 'post_id', onDelete: 'CASCADE' });
 
-module.exports = { User, Blog };
+Comment.belongsTo(User, { foreignKey: 'user_id' });
+Comment.belongsTo(Post, { foreignKey: 'post_id' });
+
+module.exports = { User, Post, Comment };
