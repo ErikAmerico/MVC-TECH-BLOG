baaa09d621b42c15d2ddd5dd64e4a823f7a9471b
Erik Olson <erikamerico1991@gmail.com>
2023-08-04 11:28:11 -0400

    Cleaned server up. Not sure What i was doing a couple weeks ago

--- server.js
+++ server.js
@@ -1,62 +1,46 @@
 
 const express = require('express');
 const session = require('express-session');
-const { create } = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const path = require('path');
-
-const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+
+const routes = require('./controllers');
+const sequelize = require('./config/connection');
+
 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const authRoutes = require('./controllers/authController');
-const dashboardRoutes = require('./controllers/dashboardController');
+const sess = {
+  secret: 'Lambo Porsche Civic',
+  cookie: {
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict',
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
 
-app.use(
-  session({
-    secret: 'very secret message that you cannot guess',
-    resave: false,
-    cookie: {},
-    saveUninitialized: true,
-    store: new SequelizeStore({
-        db: sequelize
-    })
-  })
-);
+app.use(session(sess));
 
-const hbs = create({
-  defaultLayout: 'main',
-  runtimeOptions: {
-    allowProtoPropertiesByDefault: true,
-    allowProtoMethodsByDefault: true,
-  }
-});
+const hbs = exphbs.create({});
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-const methodOverride = require('method-override');
-app.use(methodOverride('_method'));
-
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/public/delete-post.js', (req, res) => {
-  res.setHeader('Content-Type', 'text/javascript');
-  res.sendFile(path.join(__dirname, 'public', 'delete-post.js'));
-});
 
-
-app.use('/auth', authRoutes);
-
-
-app.use('/dashboard', dashboardRoutes);
-
-
-const routes = require('./controllers');
 app.use(routes);
 
 
